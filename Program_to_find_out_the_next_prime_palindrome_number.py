40705a6dbbb5ccc434a0754edf497683ae2c77ee
suma <109718770+suma217@users.noreply.github.com>
2022-10-29 10:14:22 +0530

    def is_prime(num):     if num<2:         return False     for i in range(2,int(n**0.5)+1):         if num%i==0:             return False     return True def reverse(num):     rev=0     while num>0:         r=num%10         rev=rev*10+r         num=num//10     return rev n=int(input()) n+=1 while True:     if n==reverse(n) and is_prime(n)==True:         print(n)         break     n+=1

---
+++ Program_to_find_out_the_next_prime_palindrome_number.py
@@ -0,0 +1,21 @@
+def is_prime(num):
+    if num<2:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if num%i==0:
+            return False
+    return True
+def reverse(num):
+    rev=0
+    while num>0:
+        r=num%10
+        rev=rev*10+r
+        num=num//10
+    return rev
+n=int(input())
+n+=1
+while True:
+    if n==reverse(n) and is_prime(n)==True:
+        print(n)
+        break
+    n+=1
