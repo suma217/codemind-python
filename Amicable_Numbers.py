1016ed4c145a4e6e36a3814ae3d8936a1236517d
suma <109718770+suma217@users.noreply.github.com>
2022-10-29 09:56:57 +0530

    def pfs(num):     fs=0     for i in range(1,num):         if num%i==0:             fs+=i     return fs a=int(input()) b=int(input()) if (pfs(b)==a and pfs(a)==b):     print('Amicable') else:     print('Not Amicable')

---
+++ Amicable_Numbers.py
@@ -0,0 +1,12 @@
+def pfs(num):
+    fs=0
+    for i in range(1,num):
+        if num%i==0:
+            fs+=i
+    return fs
+a=int(input())
+b=int(input())
+if (pfs(b)==a and pfs(a)==b):
+    print('Amicable')
+else:
+    print('Not Amicable')
